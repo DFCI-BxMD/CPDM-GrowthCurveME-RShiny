44af7b05090c6604b4672e20591afea9a662436a
lag83 <158304415+lag83@users.noreply.github.com>
2025-01-09 11:01:40 -0500

    update rds_visualizer.sh

--- CPDM-GrowthCurveME-RShiny-main/CPDM-GrowthCurveME-RShiny/rds_visualizer/rds_visualizer.sh
+++ CPDM-GrowthCurveME-RShiny-main/CPDM-GrowthCurveME-RShiny/rds_visualizer/rds_visualizer.sh
@@ -3,14 +3,12 @@
 set -eux
 main() {
  # get rds_vis app code
- mkdir rds_vizualizer
- url=https://raw.githubusercontent.com/rkafrawi/RDS_Vis_v1_1/main/Webapp_source
- wget -P rds_visualizer/ $url/DESCRIPTION $url/server.R $url/ui.R
+ mkdir CPDM_GrowthCurveME_RShiny
+ #url=https://raw.githubusercontent.com/rkafrawi/RDS_Vis_v1_1/main/Webapp_source
+ url=https://github.com/DFCI-BxMD/CPDM-GrowthCurveME-RShiny/tree/main/CPDM-GrowthCurveME-RShiny-main/CPDM-GrowthCurveME-RShiny/Webapp_source
+ wget -P CPDM_GrowthCurveME_RShiny/ $url/DESCRIPTION $url/server.R $url/ui.R
  
- # pull and run Shiny Server docker image
-#  dx download $DX_PROJECT_CONTEXT_ID:/rk_shiny/rds_vis_maria.tar.gz
-#  docker load -i rds_vis_maria.tar.gz
  
  # attach our rds_vis app's folder as a volume
- docker run --rm -p 443:3838 -v $PWD/rds_visualizer:/srv/shiny-server/ tariship/rds_vis_tarishi
+ docker run --rm -p 443:3838 -v $PWD/CPDM_GrowthCurveME_RShiny:/srv/shiny-server/ ledia/CPDM_GrowthCurveME_RShiny
 }
