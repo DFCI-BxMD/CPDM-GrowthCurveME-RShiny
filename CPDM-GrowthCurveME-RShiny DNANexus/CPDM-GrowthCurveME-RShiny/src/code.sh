746bd2650fc175b02c3d196f40ad968d3478d2d3
Ledia_gebremedhin <158304415+lag83@users.noreply.github.com>
2025-02-12 11:17:51 -0500

    Update code.sh

--- CPDM-GrowthCurveME-RShiny DNANexus/CPDM-GrowthCurveME-RShiny/src/code.sh
+++ CPDM-GrowthCurveME-RShiny DNANexus/CPDM-GrowthCurveME-RShiny/src/code.sh
@@ -6,6 +6,8 @@ mkdir rds_vizualizer
 url=https://raw.githubusercontent.com/rkafrawi/RDS_Vis_v1_1/main/Webapp_source
 wget -P rds_visualizer/ $url/DESCRIPTION $url/server.R $url/ui.R 
  
-docker load -i growthcurveme-v0.0.1.tar.gz
-docker run --rm -p 443:3838 -v $PWD/rds_visualizer:/srv/shiny-server/ growthcurveme-v0.0.1
+dx download project-GxzFbg0090gbkZk651G5J63p:/growthcurvemev0.0.1.tar.gz
+docker load -i growthcurvemev0.0.1.tar.gz
+
+docker run --rm -p 443:3838 -v $PWD:/srv/shiny-server/ growthcurveme:v0.0.1
 }
